a1c98dff5232eb70b798c3c326e592414292ea41
norbos <41130068+norbos@users.noreply.github.com>
2022-06-24 18:56:26 -0400

    Added empty components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Calendar from './Calendar';
 
 function App() {
   return (
     <div className="App">      
+      <Calendar></Calendar>
     </div>
   );
 }
---
+++ src/Calendar.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Body from "./Body";
+
+function Calendar() {
+  return (
+    <div>
+        <Header></Header>
+        <Body></Body>
+    </div>
+  );
+}
+
+export default Calendar;
