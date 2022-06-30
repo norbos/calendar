e7f2b83dc194ec5e0a38161d5bb9c442f3c44882
norbos <41130068+norbos@users.noreply.github.com>
2022-06-30 15:31:33 -0400

    Added redux store to entrypoint

--- src/redux/reducers/monthReducer.js
+++ src/redux/reducers/monthReducer.js
@@ -1,9 +1,9 @@
 import * as types from "../actions/actionTypes";
 
-export default function monthReducer(state, action) {
+export default function monthReducer(state = {}, action) {
     switch (action.type) {
         case types.UPDATE_MONTH:
-            return { ...state, month: month };
+            return { ...state, month: action.month };
         default:
             return state;
     }
